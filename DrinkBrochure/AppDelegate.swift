8f8e31839117a7aa975f0b25e0c5a920255663c5
ShengHua Wu <fantasy0404@gmail.com>
2017-04-19 11:18:41 +0800

    Define DrinkListViewController with two different modes.

--- DrinkBrochure/AppDelegate.swift
+++ DrinkBrochure/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.white
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let drinkListVC = DrinkListViewController()
+        drinkListVC.mode = .empty
+        let navigationController = UINavigationController(rootViewController: drinkListVC)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
---
+++ DrinkBrochure/DrinkList/DrinkListViewController.swift
@@ -0,0 +1,75 @@
+//
+//  DrinkListViewController.swift
+//  DrinkBrochure
+//
+//  Created by ShengHua Wu on 19/04/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+class DrinkListViewController: UIViewController {
+    // MARK: - Mode
+    enum Mode {
+        case empty
+        case normal
+    }
+    
+    // MARK: - Private Properties
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: UICollectionViewCell.description())
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
+    private lazy var emptyView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = UIColor.lightGray
+        return view
+    }()
+    
+    var mode: Mode = .normal {
+        didSet {
+            switch mode {
+            case .empty:
+                collectionView.isHidden = true
+                emptyView.isHidden = false
+            case .normal:
+                collectionView.isHidden = false
+                emptyView.isHidden = true
+            }
+        }
+    }
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        view.addSubview(emptyView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        collectionView.frame = view.bounds
+        emptyView.frame = view.bounds
+    }
+}
+
+// MARK: - Collection View Data Source
+extension DrinkListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UICollectionViewCell.description(), for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
+}
