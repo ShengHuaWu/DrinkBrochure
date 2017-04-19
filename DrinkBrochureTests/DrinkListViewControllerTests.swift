29c66c4e0320bbc2c5327b46adbee75297cc40cb
ShengHua Wu <fantasy0404@gmail.com>
2017-04-19 11:36:53 +0800

    Integrate FBSnapshotTestCase. Resolve #7.

---
+++ DrinkBrochureTests/DrinkListViewControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  DrinkListViewControllerTests.swift
+//  DrinkBrochure
+//
+//  Created by ShengHua Wu on 19/04/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import FBSnapshotTestCase
+@testable import DrinkBrochure
+
+class DrinkListViewControllerTests: FBSnapshotTestCase {
+    // MARK: - Override Methods
+    override func setUp() {
+        super.setUp()
+        
+        recordMode = false
+    }
+    
+    // MARK: - Enabled Tests
+    func testEmptyMode() {
+        let drinkListVC = DrinkListViewController()
+        
+        drinkListVC.mode = .empty
+        
+        FBSnapshotVerifyView(drinkListVC.view)
+    }
+    
+    func testNormalMode() {
+        let drinkListVC = DrinkListViewController()
+        
+        drinkListVC.mode = .normal
+        
+        FBSnapshotVerifyView(drinkListVC.view)
+    }
+}
