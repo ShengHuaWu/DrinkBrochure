44af8d1a97b7aa64c6e333a326b33f563e229b08
ShengHua Wu <fantasy0404@gmail.com>
2017-05-01 11:10:35 +0800

    Implement NotificationDescriptor and KeyboardPayload. Resolve #10.

---
+++ DrinkBrochure/Common/Notification/NotificationDescriptor.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationDescriptor.swift
+//  DrinkBrochure
+//
+//  Created by ShengHua Wu on 01/05/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+struct NotificationDescriptor<Payload> {
+    let name: Notification.Name
+    let convert: (Notification) -> Payload
+}
+
+extension NotificationCenter {
+    func addObserver<Payload>(with descriptor: NotificationDescriptor<Payload>, block: @escaping (Payload) -> ()) {
+        addObserver(forName: descriptor.name, object: nil, queue: nil) { (note) in
+            block(descriptor.convert(note))
+        }
+    }
+}
+
+extension UIViewController {
+    static let keyboardWillShow = NotificationDescriptor(name: Notification.Name.UIKeyboardWillShow, convert: KeyboardPayload.init)
+    static let keyboardWillHide = NotificationDescriptor(name: Notification.Name.UIKeyboardWillHide, convert: KeyboardPayload.init)
+}
