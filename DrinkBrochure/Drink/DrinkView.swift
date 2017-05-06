6c8f355166a72841d36a6a560d0e03febc049be9
ShengHua Wu <fantasy0404@gmail.com>
2017-05-06 09:40:32 +0800

    Rearrange the layout of DrinkView.

--- DrinkBrochure/Drink/DrinkView.swift
+++ DrinkBrochure/Drink/DrinkView.swift
@@ -10,24 +10,51 @@ import UIKit
 
 final class DrinkView: UIScrollView {
     // MARK: - Properties
-    private lazy var containerView: UIStackView = {
-        let elements: [ContentElement] = [
-            .image(image: UIImage()),
-            .textField(text: "", placeholder: "Drink Name"),
-            .label(text: "Category Selection"),
-            .label(text: "Rating Selection"),
-            .textView(text: "Comment"),
-            .button(title: "Delete Drink")
-        ]
-        let view = UIStackView(elements: elements)
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .brown
+        return imageView
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .brown
+        return textField
+    }()
+    
+    private lazy var categoryButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .brown
+        return button
+    }()
+    
+    private lazy var ratingView: RatingView = {
+        let view = RatingView(frame: .zero)
         return view
+    }()
+    
+    private lazy var textView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .brown
+        return textView
+    }()
+    
+    private lazy var deleteButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .brown
+        return button
     }()
     
     // MARK: - Designated Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
                 
-        addSubview(containerView)
+        addSubview(imageView)
+        addSubview(textField)
+        addSubview(categoryButton)
+        addSubview(ratingView)
+        addSubview(textView)
+        addSubview(deleteButton)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -38,6 +65,19 @@ final class DrinkView: UIScrollView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        containerView.frame = frame
+        let interval: CGFloat = 8.0
+        let middleLayout = VerticalLayout(contents: [textField, categoryButton, ratingView], spacing: interval)
+        let verticalLayout = VerticalLayout(contents: [imageView, middleLayout, textView], spacing: interval)
+        
+        let margin: CGFloat = 16.0
+        let composedLayout = InsetLayout(content: verticalLayout, inset: UIEdgeInsets(top: margin, left: margin, bottom: 0.0, right: margin))
+        
+        let height: CGFloat = 600.0
+        composedLayout.layout(in: CGRect(x: 0.0, y: 0.0, width: frame.width, height: height))
+        
+        deleteButton.frame = CGRect(x: textView.frame.minX, y: textView.frame.maxY + interval, width: categoryButton.frame.width, height: categoryButton.frame.height)
+        
+        let bottomView = deleteButton.isHidden ? textView : deleteButton
+        contentSize = CGSize(width: frame.width, height: bottomView.frame.maxY + margin)
     }
 }
